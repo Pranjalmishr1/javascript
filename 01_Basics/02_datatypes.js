892280447a864bfb0bc737c9b2e472d2ffe7a0ec
Pranjalmishr1 <24pranjalmishra2005@gmail.com>
2024-07-21 18:15:57 +0000

    Add basic JavaScript examples for variables and datatypes
    - Created 01_variables.js to demonstrate variable declarations.
    - Created 02_datatypes.js to explain different data types.
    - Deleted test.js as it was no longer needed.

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all code as newer version 
+// alert(3+3)// we are using nodejs not browser
+console.log(3
+    +3)//code readibility should be high
+    console.log("pranjal")
+let name="pranjal"
+let age=18
+let isLoggedIn= false;
+//number is start from 2 to power 23
+//bigint
+//string=>""
+//boolean=>true/fase
+//null =>standalone value also a datatype
+//undefined=> variable define kiya hai lekin koi value nahi
+//symbol=>uniqueness
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
