ff0904e63ec7acb0a75eb4c0c8829c3225bb3749
Pranjalmishr1 <24pranjalmishra2005@gmail.com>
2024-07-22 17:32:01 +0000

    conversionof datatypes

---
+++ 01_Basics/03_conversionoperations.js
@@ -0,0 +1,22 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber =Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+let sdcore =null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuesInNumber =Number(sdcore);
+console.log(typeof valuesInNumber)
+console.log(valuesInNumber);
+
+let isLoggedIn=""
+let booleanisLoggedIn= Boolean (isLoggedIn)
+console.log(booleanisLoggedIn)
